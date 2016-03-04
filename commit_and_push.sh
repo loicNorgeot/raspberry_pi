d7aab3192f72f494830d768b8afa1a7c8a424a12
loicNorgeot <loic.norgeot@gmail.com>
2016-03-04 22:46:06 +0000

    "backup_20160304_22h46_raspberrypi@192.168.0.12/24"

--- commit_and_push.sh
+++ commit_and_push.sh
@@ -1,5 +1,14 @@
+#!/bin/bash
 cd $1
+
+var=`ip addr show eth0 | grep inet`
+vars=($var)
+ip=${vars[1]}
+da=`date +%Y%m%d_%Hh%M`
+ho=`hostname`
+
 git add .
-git commit -m \"backup-`date +%Y-%m-%d-%H:%M:%S`\"
+git add -u
+git commit -m \"backup_"$da"_"$ho"@"$ip"\"
 git push -u origin master
-#echo git commit -m \"backup-`date +%Y-%m-%d-%H:%M:%S`\"
+#echo git commit -m \"backup_"$da"_"$ho"@"$ip"\"
